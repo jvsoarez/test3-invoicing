aed72ab3fead52c77aba4aa7a80d974e9eb2e7b7
jvsoarez <joaovictorcpsa@gmail.com>
2023-03-01 17:14:07 -0300

    feat: function that returns the number of the days that billing was greater than the average

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ const monthlyData = require('./data/monthlyData')
 
 const dayWithSmallerRevenue = (data) => {
   const day = data.reduce((acc, current) => current.valor > 0 && (current.valor < acc.valor) ? current : acc);
-  return `O dia ${day.dia} foi o dia de funcionamento com menor faturamento no mês, tendo faturado R$${day.valor}`;
+  return `O dia ${day.dia} foi o dia de funcionamento com menor faturamento no mês, tendo faturado R$${day.valor}.`;
 }
   
 console.log(dayWithSmallerRevenue(monthlyData));
@@ -17,10 +17,22 @@ console.log(dayWithSmallerRevenue(monthlyData));
 
 const dayWithHighestRevenue = (data) => {
   const day = data.reduce((acc, current) => current.valor > acc.valor ? current : acc);
-  return `O dia ${day.dia} foi o dia de funcionamento com menor faturamento no mês, tendo faturado R$${day.valor}`;
+  return `O dia ${day.dia} foi o dia de funcionamento com menor faturamento no mês, tendo faturado R$${day.valor}.`;
 }
   
 console.log(dayWithHighestRevenue(monthlyData));
 // Saída no console
 // O dia 16 foi o dia de funcionamento com menor faturamento no mês, tendo faturado R$48924.2448;
 
+// • Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.
+
+const dayWithHigherThanAverageRevenue = (data) => {
+  const sumValueOfAllDay = data.reduce((acc, current) => acc + current.valor, 0);
+  const average = sumValueOfAllDay / data.length;
+  const days = data.filter((day) => day.valor > average);
+  return `No total, foram ${days.length} dias com o faturamento maior que a média do mês.`;
+}
+
+console.log(dayWithHigherThanAverageRevenue(monthlyData));
+// Saída no console
+// No total, foram 12 dias com o faturamento maior que a média do mês.
