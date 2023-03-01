4df87d96b46029047dc745882da85b365b5117f1
jvsoarez <joaovictorcpsa@gmail.com>
2023-03-01 17:01:41 -0300

    feat: function that returns the highest billing that occurred in the month

--- src/script.js
+++ src/script.js
@@ -1,5 +1,8 @@
 const monthlyData = require('./data/monthlyData')
 
+// • O menor valor de faturamento ocorrido em um dia do mês;
+
+
 const dayWithSmallerRevenue = (data) => {
   const day = data.reduce((acc, current) => current.valor > 0 && (current.valor < acc.valor) ? current : acc);
   return `O dia ${day.dia} foi o dia de funcionamento com menor faturamento no mês, tendo faturado R$${day.valor}`;
@@ -8,3 +11,16 @@ const dayWithSmallerRevenue = (data) => {
 console.log(dayWithSmallerRevenue(monthlyData));
 // Saída no console
 // O dia 14 foi o dia de funcionamento com menor faturamento no mês, tendo faturado R$373.7838;
+
+
+// • O maior valor de faturamento ocorrido em um dia do mês;
+
+const dayWithHighestRevenue = (data) => {
+  const day = data.reduce((acc, current) => current.valor > acc.valor ? current : acc);
+  return `O dia ${day.dia} foi o dia de funcionamento com menor faturamento no mês, tendo faturado R$${day.valor}`;
+}
+  
+console.log(dayWithHighestRevenue(monthlyData));
+// Saída no console
+// O dia 16 foi o dia de funcionamento com menor faturamento no mês, tendo faturado R$48924.2448;
+
